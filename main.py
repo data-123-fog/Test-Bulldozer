f5fc3b5edb7afe5a902ccf4da1193377082918f5
data-123-fog <lpoko5468@gmail.com>
2026-08-15 10:01:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Привет! Я программа, которую собрал бульдозер GitHub Actions!")
-input("Нажми Enter, чтобы выйти...")
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Привет, Миша! Это APK собранный на GitHub!')
+
+if __name__ == '__main__':
+    MyApp().run()
+  
